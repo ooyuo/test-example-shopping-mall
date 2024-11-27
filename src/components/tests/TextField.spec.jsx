8a65da402b59de8b4f502d1c3151a858a6134c19
doyunj <doyunj@modeal.net>
2024-11-27 15:38:25 +0900

    test: unit test

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -4,6 +4,21 @@ import React from 'react';
 import TextField from '@/components/TextField';
 import render from '@/utils/test/render';
 
-it('sample test.', async () => {
-  expect(1).toBe(1);
+it('className prop으로 설정한 css class가 적용된다.', async () => {
+  // AAA
+  // Arrange -> Act -> Assert
+
+  // #1. Arrange
+  // render API 호출 -> 테스트 환경의 jsDOM에 리액트 컴포넌트가 랜더링된 DOM 구조가 반영
+  // jsDOM: Node.js에서 사용하기 위해 많은 웹 표준을 순수 자바스크립트로 구현
+  await render(<TextField className="my-class" />);
+
+  // #2. Assert
+  // vitest의 expect 함수를 사용하여 기대 결과를 검증
+
+  // className이란 내부 prop이나 state 값을 검증 (X)
+  // 랜더링되는 DOM 구조가 올바르게 변경되었는지 확인 (O) -> 최종적으로 사용자가 보는 결과는 DOM
+  expect(screen.getByPlaceholderText('텍스트를 입력해 주세요.')).toHaveClass(
+    'my-class',
+  );
 });
